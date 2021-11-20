85777eaf2a1aade3af1a19e0c7b9743bf6ebbf17
Pedro Henrique Dionisio <pedrohenriquedionisioalves@gmail.com>
2021-11-19 23:49:31 -0300

    chore: configure theming with theme provider

--- desafio/src/App.js
+++ desafio/src/App.js
@@ -1,11 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import defaultTheme from './assets/styles/themes/default'
+
 import GlobalStyles from './assets/styles/global'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
       <h1>James tip</h1>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ desafio/src/assets/styles/themes/default.js
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    default: {
+      white: '#FDFDFD',
+      black: '#101010',
+    },
+    accent: {
+      blue: '#31F7DC',
+      red: '#F26767',
+      green: '#61CE70',
+    },
+    gray: {
+      100: '#ACB5BD',
+      200: '#495057',
+    },
+  },
+}
