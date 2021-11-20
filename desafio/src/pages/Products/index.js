a154d51d771ecb311f835d20b698bd96c421d9a1
Pedro Henrique Dionisio <pedrohenriquedionisioalves@gmail.com>
2021-11-20 15:32:09 -0300

    feat: create page Products

--- desafio/src/pages/Products/index.js
+++ desafio/src/pages/Products/index.js
@@ -1,5 +1,47 @@
+import { ProductsHeader, Wrapper } from './styles'
+
+import arrow from '../../assets/styles/images/arrow-left.svg'
+import ProductCard from '../../components/ProductCard'
+
 export default function Products() {
   return (
-    <h1>Products</h1>
+    <Wrapper>
+      <ProductsHeader>
+        <img src={arrow} alt="Voltar" />
+        <h1>Produtos cadastrados</h1>
+      </ProductsHeader>
+
+      <ProductCard
+        product="Notebook"
+        code="12364"
+        category="Eletrônicos"
+        provider="Dell"
+        value="1256.40"
+      />
+
+      <ProductCard
+        product="Notebook"
+        code="12364"
+        category="Eletrônicos"
+        provider="Dell"
+        value="1256.40"
+      />
+
+      <ProductCard
+        product="Notebook"
+        code="12364"
+        category="Eletrônicos"
+        provider="Dell"
+        value="1256.40"
+      />
+
+      <ProductCard
+        product="Notebook"
+        code="12364"
+        category="Eletrônicos"
+        provider="Dell"
+        value="1256.40"
+      />
+    </Wrapper>
   )
 }
---
+++ desafio/src/pages/Products/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+
+export const ProductsHeader = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 32px;
+
+  img {
+    margin-right: 8px;
+    cursor: pointer;
+  }
+
+  h1 {
+    font-weight: normal;
+  }
+`
