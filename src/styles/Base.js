00cd48696b0091ed4073ecc4356f5fe270d8910d
DyogoViana <dyogo.vianaa@gmail.com>
2022-06-09 12:50:09 -0300

    // ajuste

---
+++ src/styles/Base.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Base = createGlobalStyle`
+    body {
+      font-family: 'Quicksand', sans-serif;
+		background-color: #fff;
+		font-weight: lighter;
+		color: #404040;
+    }
+
+	button {
+	cursor: pointer;
+	 }
+`;
+
+export default Base;
