6b407a9b0c1ae8c84b4db0a7085ffe7605bf110c
Filipe Batista <filipe.x@live.com>
2022-06-09 16:24:53 -0300

    criação do contexto

--- src/context/ProductContext.tsx
+++ src/context/ProductContext.tsx
@@ -1,15 +1,84 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
-export const ProductContext = createContext(null);
+const ProductContext = createContext({} as any);
 
 type ProductProvider = {
   children: ReactNode;
+};
+
+interface ProductInterface {
+  id: string;
+  name: string;
+  provider: string;
+  category: string;
+  value: string;
 }
 
 const ProductProvider = (props: ProductProvider) => {
-  return(
-  <ProductContext.Provider value={null}>
-    {props.children}
+  const [products, setProducts] = useState<ProductInterface[]>(
+    [] as ProductInterface[]
+  );
+  const [selectedProduct, setSelectedProduct] = useState<ProductInterface>(
+    {} as ProductInterface
+  );
+  
+  function removeItem(data: ProductInterface) {
+    const newProductsArray = products.filter(
+      (product) => data.id !== product.id
+    );
+
+    localStorage.setItem("data", JSON.stringify(newProductsArray));
+    setProducts(newProductsArray);
+
+    alert("Produto removido com sucesso!");
+  }
+
+  function updateItem(data: ProductInterface) {
+    const newProductsArray = products.filter(
+      (product) => data.id !== product.id
+    );
+    newProductsArray.push(data);
+
+    localStorage.setItem("data", JSON.stringify(newProductsArray));
+    setProducts(newProductsArray);
+
+    alert("Produto atualizado com sucesso!");
+  }
+
+  function addItem(data: ProductInterface) {
+    const newProductsArray = [...products, data];
+    localStorage.setItem("data", JSON.stringify(newProductsArray));
+    setProducts(newProductsArray);
+    alert("Produto adicionado com sucesso!");
+  }
+
+  function loadTable() {
+    const localStorageData = localStorage.getItem("data");
+
+    if (localStorageData) {
+      setProducts(JSON.parse(localStorageData));
+    } else {
+      localStorage.setItem("data", "[]");
+    }
+  }
+
+  useEffect(() => {
+    loadTable();
+  }, []);
+
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        setProducts,
+        selectedProduct,
+        setSelectedProduct,
+        addItem,
+        updateItem,
+        removeItem
+      }}
+    >
+      {props.children}
     </ProductContext.Provider>
   );
 };
