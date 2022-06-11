39b88fd8be47926dbb08d4cc460651216ec4e4be
Anderson Apparecido <anderson@bemobile.tech>
2022-06-11 19:08:42 -0300

    feat: responsive grid

--- packages/products/src/components/App/styles.ts
+++ packages/products/src/components/App/styles.ts
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
+  max-width: 990px;
   height: calc(100vh - 92px);
   margin: 0 auto;
   padding: 0 24px;
-  /* background: ${({ theme }) => theme.colors.background}; */
 `;
--- packages/products/src/pages/Home/styles.ts
+++ packages/products/src/pages/Home/styles.ts
@@ -1,11 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding-top: 60px;
+  padding-top: 36px;
+
+  .button-wrapper {
+    margin: 0 auto;
+    width: 240px;
+    margin-bottom: 24px;
+  }
 `;
 
 export const ProductSection = styled.section`
   display: grid;
-  grid-template-columns: repeat(4, 300px);
+  justify-items: center;
+  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
   gap: 20px;
 `;
