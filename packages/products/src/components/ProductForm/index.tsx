eacb0c882c114d3232fb5b266b969e88f11c8d48
Anderson Apparecido <anderson@bemobile.tech>
2022-06-12 17:20:14 -0300

    feat: error hook created

--- packages/products/src/components/ProductForm/index.tsx
+++ packages/products/src/components/ProductForm/index.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+
 import { Product } from "../../@types/Product";
 
 import { Button } from "../Button";
 import { FormGroup } from "../FormGroup";
 import Input from "../Input";
 import Select from "../Select";
+
+import useErrors from "../../hooks/useErrors";
 
 import { Form, ButtonContainer } from "./styles";
 
@@ -21,24 +24,59 @@ export function ProductForm({ onSubmit, title, data }: ProductFormProps) {
   const [provider, setProvider] = useState(data?.provider ?? "");
   const [price, setPrice] = useState(data?.price ?? "");
 
+  const { errors, setError, removeError, getErrorMessageByFieldName } =
+    useErrors();
+
+  const isFormValid = name && errors.length === 0;
+
   function handleCodeChange(event) {
     setCode(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: "code", message: "Código é obrigatório" });
+    } else {
+      removeError("code");
+    }
   }
 
   function handleNameChange(event) {
     setName(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: "name", message: "Nome é obrigatório" });
+    } else {
+      removeError("name");
+    }
   }
 
   function handleCategoryChange(event) {
     setCategory(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: "category", message: "Categoria é obrigatória" });
+    } else {
+      removeError("category");
+    }
   }
 
   function handleProviderChange(event) {
     setProvider(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: "provider", message: "Fornecedor é obrigatório" });
+    } else {
+      removeError("provider");
+    }
   }
 
   function handlePriceChange(event) {
     setPrice(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: "price", message: "Preço é obrigatório" });
+    } else {
+      removeError("price");
+    }
   }
 
   function handleSubmit() {
@@ -51,16 +89,25 @@ export function ProductForm({ onSubmit, title, data }: ProductFormProps) {
         <h1>{title}</h1>
       </div>
 
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName("code")}>
         <span>Código</span>
-        <Input type="number" value={code} onChange={handleCodeChange} />
+        <Input
+          type="number"
+          value={code}
+          onChange={handleCodeChange}
+          error={getErrorMessageByFieldName("code")}
+        />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName("name")}>
         <span>Nome do produto</span>
-        <Input value={name} onChange={handleNameChange} />
+        <Input
+          value={name}
+          onChange={handleNameChange}
+          error={getErrorMessageByFieldName("name")}
+        />
       </FormGroup>
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName("category")}>
         <span>Categoria</span>
         <Select value={category} onChange={handleCategoryChange}>
           <option value="">Sem categoria</option>
@@ -68,7 +115,7 @@ export function ProductForm({ onSubmit, title, data }: ProductFormProps) {
           <option value="eletrodomésticos">Eletrodomésticos</option>
         </Select>
       </FormGroup>
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName("provider")}>
         <span>Fornecedor</span>
         <Select value={provider} onChange={handleProviderChange}>
           <option value="">Sem fornecedor</option>
@@ -77,9 +124,13 @@ export function ProductForm({ onSubmit, title, data }: ProductFormProps) {
         </Select>
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName("price")}>
         <span>Preço</span>
-        <Input value={price} onChange={handlePriceChange} />
+        <Input
+          value={price}
+          onChange={handlePriceChange}
+          error={getErrorMessageByFieldName("price")}
+        />
       </FormGroup>
 
       <ButtonContainer>
