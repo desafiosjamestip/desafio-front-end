f7469eac096e8e0a3880364859d71ae1a8ba247b
Andre <heraandre@gmail.com>
2022-06-12 01:27:29 -0300

    feat: add dependences

---
+++ src/styles/index.js
@@ -0,0 +1,82 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Reset = createGlobalStyle`
+    /* http://meyerweb.com/eric/tools/css/reset/ 
+    v2.0 | 20110126
+    License: none (public domain)
+    */
+
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed, 
+    figure, figcaption, footer, header, hgroup, 
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video, input, main, textarea {
+        font: inherit;
+        font-size: 1rem;
+        text-decoration: none;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        border: 0;
+        vertical-align: baseline;
+        box-sizing: border-box;
+    }
+    /* HTML5 display-role reset for older browsers */
+    article, aside, details, figcaption, figure, 
+    footer, header, hgroup, menu, nav, section {
+        display: block;
+    }
+    body {
+        line-height: 1;
+    }
+    ol, ul {
+        list-style: none;
+    }
+    blockquote, q {
+        quotes: none;
+    }
+    blockquote:before, blockquote:after,
+    q:before, q:after {
+        content: '';
+        content: none;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+`;
+
+export const RootVariables = createGlobalStyle`
+    :root{
+        //THEME COLORS
+        --color-primary: #6EC1E4;
+        --color-secondary-1: #54595F;
+        --color-secondary-2: #101010;
+
+        //GENERAL COLORS
+        --color-white: #FFFFFF;
+        --color-black: #000000;
+
+    }
+`;
+
+export const GlobalStyles = createGlobalStyle`
+    body { 
+        font: var(--text-12);
+        background: var(--color-background);
+        color: var(--grey-4);
+        min-height: 100vh;
+    }
+
+    button {
+        cursor: pointer;
+    }
+`;
