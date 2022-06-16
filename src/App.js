4091e0cbdb5d828f82e1d0db2bc8b8a9c94671b0
Andre <heraandre@gmail.com>
2022-06-15 21:05:59 -0300

    feat: updating files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Routes from "./router";
 
+import { Reset, RootVariables, GlobalStyles } from "./styles";
+
 function App() {
   return (
     <div className="App">
+      <Reset />
+      <RootVariables />
+      <GlobalStyles />
+
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
