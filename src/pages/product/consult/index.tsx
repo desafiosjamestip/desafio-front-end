ceff1c91c1eb46d188657e03f00bc8e310174a06
Fábio Hideaki Matsumoto <hide.fabio123@gmail.com>
2022-06-16 21:40:51 -0300

    feat: create products list, buttons and their functions

--- src/pages/product/consult/index.tsx
+++ src/pages/product/consult/index.tsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React from 'react'
+import useProducts from '../../../hooks/useProducts'
+import { ProductProps } from '../../../contexts/ProductContext'
+import restaure from '../../../repo'
 
 import { Container } from './styles'
 
 const Consult: React.FC = () => {
+    const { productsList, removeProduct } = useProducts()
+
+    const handleRemoveProduct = (props: ProductProps) => {
+        const newProductList = productsList.filter((prod) => prod.id !== props.id)
+        removeProduct(newProductList)
+    }
+
+    const restaures = () => {
+        console.log('dhbasjkhkjdsa')
+        localStorage.setItem('@jamestips:products-list', JSON.stringify(restaure))
+    }
 
     return (
-        <Container><h1>CONSULT PAGE</h1></Container>
+        <Container>
+            <button onClick={restaures}>RESTORE</button>
+            <table style={{border: '1px solid'}}>
+                <thead>
+                    <tr >
+                        <th style={{border: '1px solid'}}>ID</th>
+                        <th style={{border: '1px solid'}}>Nome</th>
+                        <th style={{border: '1px solid'}}>Categoria</th>
+                        <th style={{border: '1px solid'}}>Fornecedor</th>
+                        <th style={{border: '1px solid'}}>Valor</th>
+                        <th style={{border: '1px solid'}}>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {productsList.map((prod) => (
+                        <tr key={prod.id}>
+                            <td style={{border: '1px solid'}}>{prod.id}</td>
+                            <td style={{border: '1px solid'}}>{prod.name}</td>
+                            <td style={{border: '1px solid'}}>{prod.category}</td>
+                            <td style={{border: '1px solid'}}>{prod.provider}</td>
+                            <td style={{border: '1px solid'}}>{prod.price}</td>
+                            <td><button>EDITAR</button> <button type="button"
+                                onClick={() => handleRemoveProduct(prod)}>EXCLUIR</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+
+
+            </table>
+        </Container>
     )
 }
 
