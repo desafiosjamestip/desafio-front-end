0f3e9677ae3518f127b0987fbbaad3caf4b1b98b
Fábio Hideaki Matsumoto <hide.fabio123@gmail.com>
2022-06-19 19:04:52 -0300

    feat: add input currency mask and improvement buttons function

--- src/pages/product/register/index.tsx
+++ src/pages/product/register/index.tsx
@@ -1,88 +1,111 @@
-import React, { useState, FormEvent } from 'react'
+import React, { useState, FormEvent, useEffect, useMemo } from 'react'
+import { useParams } from 'react-router'
 import { nanoid } from 'nanoid'
 import useProducts from '../../../hooks/useProducts'
-import { Container, Header, Content, Footer } from './styles'
+import formatCurrency, { unformatCurrency } from '../../../utils/formatCurrency'
+import { Container, Header, Footer, Form } from './styles'
 import { MdOutlineSaveAlt } from 'react-icons/md'
 import theme from '../../../styles/themes/dark'
 import Input from '../../../components/Input'
 import Button from '../../../components/Button'
+import MessageBox from '../../../components/MessageBox'
 
-const initialState = () => {
-    return {
-        id: nanoid(6),
-        name: '',
-        category: '',
-        provider: '',
-        price: '',
-        created_at: '',
-        updated_at: ''
-    }
+const initialState = {
+    id: nanoid(6),
+    name: '',
+    category: '',
+    provider: '',
+    price: '0',
+    created_at: '',
+    updated_at: ''
 }
 
 const Register: React.FC = () => {
-    const [product, setProduct] = useState(initialState())
-    const { productsList, createProduct } = useProducts()
+    const [product, setProduct] = useState(initialState)
+    const { selectProductToUpate, selectedProduct, createProduct, updateProduct, message } = useProducts()
+    const { currentURL } = useParams()
+    const createForm = currentURL === "create"
+
+    const formattedCurrency = useMemo(() => {
+        return formatCurrency(product.price)
+    }, [product.price])
 
     const onChange = (event: FormEvent<HTMLInputElement>): void => {
-        const { name, value } = event.currentTarget
+        let { name, value } = event.currentTarget
         setProduct({
             ...product,
-            [name]: value
+            [name]: value,
         })
     }
 
+    useEffect(() => {
+        selectedProduct && setProduct(selectedProduct)
+    }, [currentURL])
+
+    const handleUnformatCurrency = () =>
+        product.price = unformatCurrency(product.price)
+
     const handleAddItem = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
+        handleUnformatCurrency()
         createProduct(product)
+        clearInputs()
+    }
+
+    const handleUpdateItem = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        updateProduct(product)
+        clearInputs()
+    }
+
+    const clearInputs = () => {
         setProduct(initialState)
+        selectProductToUpate(initialState)
     }
 
     return (
         <Container>
             <Header>
-                <h2>Adicionar novo produto</h2>
-                <p>Adicione seus produtos informando os dados abaixo:</p>
+                <h2>{`${createForm ? 'Adicionar novo' : 'Editar'} produto`}</h2>
+                <p>{createForm
+                    ? 'Adicione seus produtos informando os dados abaixo:'
+                    : 'Abaixo estão os valores originais do produto. Para editar os dados, altere os seus valores e clique em Salvar.'}</p>
             </Header>
-            <Content>
-                <form onSubmit={handleAddItem}>
-                    <Input
-                        required name="name"
-                        placeholder="Nome do produto"
-                        color={theme.colors.title}
-                        onChange={onChange}
-                        value={product.name}
-                    ></Input>
-                    <Input
-                        required name="category"
-                        placeholder="Categoria"
-                        onChange={onChange}
-                        value={product.category}
-                    ></Input>
-                    <Input
-                        required name="provider"
-                        placeholder="Fornecedor"
-                        onChange={onChange}
-                        value={product.provider}
-                    ></Input>
-                    <Input
-                        required name="price"
-                        placeholder="Valor do produto"
-                        onChange={onChange}
-                        value={product.price}
-                    ></Input>
-                    <Footer>
-                        <Button
-                            name="save"
-                            label="Adicionar"
-                            type="submit"><MdOutlineSaveAlt /></Button>
-                        <Button
-                            name="cancel"
-                            label="Cancelar"
-                            type="button"
-                            color={theme.colors.decline}></Button>
-                    </Footer>
-                </form>
-            </Content>
+            <Form onSubmit={createForm ? handleAddItem : handleUpdateItem}>
+                <Input
+                    required name="name"
+                    placeholder="Nome do produto"
+                    color={theme.colors.title}
+                    onChange={onChange}
+                    value={product.name}
+                ></Input>
+                <Input
+                    required name="category"
+                    placeholder="Categoria"
+                    onChange={onChange}
+                    value={product.category}
+                ></Input>
+                <Input
+                    required name="provider"
+                    placeholder="Fornecedor"
+                    onChange={onChange}
+                    value={product.provider}
+                ></Input>
+                <Input
+                    required name="price"
+                    placeholder="Valor do produto"
+                    onChange={onChange}
+                    value={formattedCurrency}
+                ></Input>
+                <Footer>
+                    <Button
+                        name="save"
+                        label={createForm ? 'Adicionar' : 'Salvar'}
+                        type="submit"><MdOutlineSaveAlt /></Button>
+
+                </Footer>
+                <MessageBox message={message} />
+            </Form>
         </Container>
     )
 }
