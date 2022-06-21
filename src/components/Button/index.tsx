dd95f986472b78e92b4f2dfb5fd44193ff79705e
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-06-21 16:23:55 -0300

    feat: Button component working with S-C props.

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+import { ButtonStyled } from "./styled";
+
+interface ReactProps {
+    children: ReactNode;
+    confirm?: boolean;
+    newItem? : boolean;
+    onClick: () => void;
+}
+
+const Button = ({ children, confirm, newItem, onClick }: ReactProps) => {
+
+    return (
+        <ButtonStyled confirm={confirm} newItem={newItem} onClick={onClick}>
+            {children}
+        </ButtonStyled>
+    )
+}
+
+export default Button;
---
+++ src/components/Button/styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ButtonStyled = styled.button<{
+    confirm?: boolean,
+    newItem?: boolean}>`
+
+    padding: ${(props) => props.confirm ? "0 50px" : "0 24px"};
+    height: ${(props) => props.confirm ? "50px" : props.newItem ? "45px": "55px"};
+    background: ${(props) => props.confirm ? "var(--darkish-color)" : "var(--blueish-color)"};
+    color: ${(props) => props.confirm ? "var(--blueish-color)" : props.newItem ? "var(--whiteish-color)": "var(--darkish-color)"};
+    border-radius: 24px;
+    font-size: ${(props) => props.confirm || props.newItem ? "20px" : "24px"};
+    border: transparent;
+    font-weight: 800;
+    cursor: pointer;
+    transition: all 0.4s;
+    &:hover {
+        background: ${(props) => props.confirm ? "var(--darkish-color)" : "var(--whiteish-color)"};
+        color: ${(props) => props.confirm ? "var(--blueish-color)" : "var(--blueish-color)"};
+    }
+`
